c67abfcc333e5d35693656482e24ad3847cb1384
Arturo Burigo <arturoburigo@Arturos-MacBook-Pro.local>
2023-07-27 22:59:11 -0300

    final

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -2,18 +2,59 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { VStack, Text, ScrollView, Center, Skeleton, Heading } from "native-base";
+import { VStack, Text, ScrollView, Center, Skeleton, Heading, Toast } from "native-base";
 import { useState } from "react";
 import * as ImagePicker from 'expo-image-picker'
-import { TouchableOpacity } from "react-native";
+import * as FileSystem from 'expo-file-system'
+import { Alert, TouchableOpacity } from "react-native";
 
 const PHOTO_SIZE = 33
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
+  const [userPhoto, setUserPhoto] = useState("https://github.com/arturoburigo.png")
 
   async function handleUserPhotoSelect() {
-    await ImagePicker.launchImageLibraryAsync();
+    setPhotoIsLoading(true)
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4,4],
+        allowsEditing: true,
+        selectionLimit: 1
+      });
+  
+      console.log(photoSelected)
+  
+      if(photoSelected.canceled){
+        return
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri) as FileSystem.FileInfo
+
+        if(photoInfo.size && (photoInfo.size / 1024 / 1024) > 5 ) {
+          return Toast.show({
+            title: "Select an Image with less than 5MB",
+            placement: 'top',
+            bgColor: 'red.500'
+          })
+        } else {
+            Toast.show({
+            title: "You changed your image profile",
+            placement: 'top',
+            bgColor: 'green.500'
+          })
+        }
+        
+        setUserPhoto(photoSelected.assets[0].uri)
+      }
+    } catch (error){
+      console.log(error)
+    } finally {
+      setPhotoIsLoading(false)
+    }
   }
 
   return (
@@ -32,7 +73,7 @@ export function Profile() {
                 />
                 :
                 <UserPhoto 
-                  source={{ uri: "https://github.com/arturoburigo.png" }} 
+                  source={{ uri: userPhoto }} 
                   size={PHOTO_SIZE} 
                   alt="User Photo" 
                 />
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,17 +1,41 @@
 import { Center, VStack, Image, Text, Heading, ScrollView } from "native-base";
+import {Controller, useForm} from 'react-hook-form'
+import { useNavigation } from "@react-navigation/native";
+import {yupResolver} from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
 import LogoImg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
-import { useNavigation } from "@react-navigation/native";
 import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+
+type FormDataProps = {
+  name: string
+  password: string
+  confirmPassword: string
+  email: string
+}
+
+const signUpSchema = yup.object({
+  name: yup.string().required('Inform a name'),
+  email: yup.string().required('Inform an e-mail').email('E-mail invalid'),
+  password: yup.string().required('Inform a password').min(6, 'Password must be at least 6 words'),
+  confirmPassword: yup.string().required('Inform a password').oneOf([yup.ref('password')], 'Password does not match')
+})
 
 export function SingUp() {
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
+  const {control, handleSubmit, formState: {errors}} = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema)
+  })
 
   function handleNavigateSignIn() {
     navigation.navigate("signIn");
+  }
+
+  function handleSignUp(data: FormDataProps) {
+    console.log(data)
   }
 
   return (
@@ -37,15 +61,68 @@ export function SingUp() {
             Acess your account
           </Heading>
 
-          <Input placeholder="Your Name" keyboardType="default" />
+          <Controller
+            name="name"
+            control={control}
+            render={({ field: {onChange, value}}) => (              
+              <Input 
+                placeholder="Your Name" 
+                keyboardType="default" 
+                onChangeText={onChange} 
+                value={value}
+                errorMessage={errors.name?.message}
+              />
+            )}
+          />
 
-          <Input placeholder="E-mail" keyboardType="email-address" autoCapitalize="none" />
+          <Controller
+            name="email"
+            control={control}
+            render={({ field: {onChange, value}}) => (              
+              <Input 
+                placeholder="E-mail" 
+                keyboardType="email-address" 
+                autoCapitalize="none"
+                onChangeText={onChange} 
+                value={value}
+                errorMessage={errors.email?.message}
+              />
+            )}
+          />
 
-          <Input placeholder="Password" secureTextEntry autoCapitalize="none" />
+          <Controller
+            name="password"
+            control={control}
+            render={({ field: {onChange, value}}) => (              
+              <Input 
+                placeholder="Password" 
+                secureTextEntry
+                autoCapitalize="none"
+                onChangeText={onChange} 
+                value={value}
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
 
-          <Input placeholder="Confirm Password" secureTextEntry autoCapitalize="none" />
+          <Controller
+            control={control}
+            name="confirmPassword"
+            render={({ field: {onChange, value}}) => (              
+              <Input 
+                placeholder="Confirm Password" 
+                secureTextEntry
+                autoCapitalize="none"
+                onChangeText={onChange} 
+                value={value}
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                returnKeyType="send"
+                errorMessage={errors.confirmPassword?.message}
+              />
+            )}
+          />
 
-          <Button title="I'm ready 💪🏻🏋🏻‍♀️" />
+          <Button title="I'm ready 💪🏻🏋🏻‍♀️" onPress={handleSubmit(handleSignUp)} />
         </Center>
 
         <Button
